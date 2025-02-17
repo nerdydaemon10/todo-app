a0e81bbc908a4a565d1d46958b6babd83cdb56c8
keannoregino <keanno.regino@radenta.com>
2025-02-17 16:36:53 +0800

    added darkMode: 'selector'

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  darkMode: 'selector', // enable dark: class
   theme: {
     extend: {},
   },
