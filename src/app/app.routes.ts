b93987bc2e2897d967c8afb5a286e626ffb8c854
keannoregino <keanno.regino@radenta.com>
2025-02-17 16:44:14 +0800

    setting-up routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    loadChildren: () => import("./auth/auth.routes").then(r => r.authRoutes)
+  },
+  {
+    path: "home",
+    loadChildren: () => import("./home/home.routes").then(r => r.homeRoutes)
+  },
+];
